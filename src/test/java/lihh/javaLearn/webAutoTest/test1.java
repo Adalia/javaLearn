7485a8c471fd5cebd2edf8abe748e628a2d0cae8
Adalia <haihui_blessed@163.com>
2020-03-15 18:52:12 +0800

    init

---
+++ src/test/java/lihh/javaLearn/webAutoTest/test1.java
@@ -0,0 +1,36 @@
+package lihh.javaLearn.webAutoTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+import org.openqa.selenium.*;
+
+public class test1
+{
+
+    WebDriver driver;
+    @Test()
+    public void shouldAnswerWithTrue()
+    {
+        WebDriverManager.chromedriver().setup();
+        String url = "file:///Users/lihaihui/Documents/CodeEnv/javaLearn/src/test/resources/relevant" +
+                ".html";
+        driver = new ChromeDriver();
+        driver.get(url);
+//        Assert.assertTrue( true );
+        WebElement input = driver.findElement(By.xpath("//div[@class='content']//ul[@class='j-group']//span[@class='name' and contains(text(),'Lucy')]/..//input"));
+
+        WebElement check =
+                driver.findElement(By.xpath("//div[contains(@class,'tl')]//li[@data-value='1']"));
+
+    }
+    @AfterTest()
+    public void close()
+    {
+        driver.quit();
+    }
+}
